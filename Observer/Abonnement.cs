1d0cb96070f3701cf9fa99b78a1e9431b7c93c84
Joelmk28 <joelmuhindok-github@example.com>
2026-06-03 23:26:33 +0100

    Creations de Observer et l'obsevable

---
+++ Observer/Abonnement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Observer
+{
+    public  interface Abonnement //Observer
+    {
+        public void Sinformer(Journaliste journaliste);//update avec la technique Pop 
+    }
+}
---
+++ Observer/Journaliste.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Observer
+{
+    public  interface Journaliste
+    {
+        public void Abonner(Abonnement abonnement); //ajout d'un observer 
+        public void Desaboner(Abonnement abonnement); //retirer un obsever
+
+        public void notifierLesAbonnees(); // envoyer les infos aux observers
+    }
+}
