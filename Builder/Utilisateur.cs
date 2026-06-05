70e2b5825299d7b55692654b2a5450c98aa28c69
Joelmk28 <joelmuhindok-github@example.com>
2026-06-05 23:59:54 +0100

    Utilisateur et Builder

---
+++ Builder/Utilisateur.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Builder
+{
+    public class Utilisateur
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Email { get; set; }
+        
+    }
+}
---
+++ Builder/UtilisateurBuilder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Builder
+{
+    public static class UtilisateurBuilder
+    {
+        public static Utilisateur Build(string nom, string prenom, string email)
+        {
+            return new Utilisateur
+            {
+                Nom = nom,
+                Prenom = prenom,
+                Email = email
+            };
+        }
+    }
+}
