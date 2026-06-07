eecec618e987126df9bf0db654495acbbe0588b9
Joelmk28 <joelmuhindok-github@example.com>
2026-06-07 23:57:20 +0100

    Creation de l'interpreteur et la classe User

---
+++ Mediator/IMediator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Mediator
+{
+    //Le Pattern Mediator permet à plusieurs objets de communiquer entre eux sans se connaitre dirrectement
+    public interface IMediator
+    {
+        void sendMessage(string message, User user);
+    }
+}
---
+++ Mediator/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace DesignPattern.Mediator
+{
+    public class User
+    {
+        private string Name {  get;}
+        private readonly IMediator imediator;
+        
+        
+        public User(string name)
+        {
+            Name = name;
+        }
+        public void sendMessage(string message, User receiver)
+        {
+            receiver.ReceiveMessage(message);
+        }
+
+        private void ReceiveMessage(string message)
+        {
+           Console.WriteLine("Message recu : "+ message + " de ma la part de " +Name);
+        }
+    }
+}
