910771e5a9a32f51c3d21b80e7750cdec60ea0bf
Joelmk28 <joelmuhindok-github@example.com>
2026-06-09 23:59:32 +0100

    init bridge et Creation de l'interface I%essageSender

---
+++ Bridge/EmailSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Bridge;
+
+    public class EmailSender : IMessageSender
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"Email envoyé : {message}");
+    }
+}
+
+ 
+
---
+++ Bridge/IMessageSender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Bridge
+{
+    public interface IMessageSender
+    {
+        void Send(string message);
+    }
+}
