5dfc66b4cee94feccb44bb0044b94b96cff17770
Joelmk28 <joelmuhindok-github@example.com>
2026-06-10 00:06:44 +0100

    WhatsAppSender

---
+++ Bridge/WhatsAppSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Bridge
+{
+    public class WhatsAppSender : IMessageSender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"WhatsApp envoyé : {message}");
+        }
+    }
+}
