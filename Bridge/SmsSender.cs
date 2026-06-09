fe48943268d142ca9d72b9b29e468da4ad9ed67a
Joelmk28 <joelmuhindok-github@example.com>
2026-06-10 00:01:47 +0100

    classe SmsSender

---
+++ Bridge/SmsSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Bridge
+{
+    public class SmsSender : IMessageSender
+    {
+        public void Send(string message)
+        {
+            Console.WriteLine($"SMS envoyé : {message}");
+        }
+    }
+}
