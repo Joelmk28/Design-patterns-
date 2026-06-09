07980695aaa3cd2fde79b1f79913b93454a4a7d2
Joelmk28 <joelmuhindok-github@example.com>
2026-06-10 00:17:02 +0100

    Abstration rafinnée en creer la classe nottification Normalle et urgenete

---
+++ Bridge/NormalNotification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Bridge
+{
+    public  class NormalNotification : Notification
+    {
+        public NormalNotification(IMessageSender sender)
+            : base(sender)
+        {
+        }
+
+        public override void Notify(string message)
+        {
+            Sender.Send(message);
+        }
+    }
+    
+}
---
+++ Bridge/UrgentNotification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.Bridge
+{
+    public class UrgentNotification : Notification
+    {
+        public UrgentNotification(IMessageSender sender)
+            : base(sender)
+        {
+        }
+
+        public override void Notify(string message)
+        {
+            Sender.Send("[URGENT] " + message);
+        }
+    }
+    
+}
